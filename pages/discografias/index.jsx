32ac311d819e1c6947a3a45435b88a69d8fdf2b4
mayconbalves <mayconbalves1@gmail.com>
2023-12-05 22:44:42 -0300

    feat: created discografy, news and playlist pages

---
+++ pages/discografias/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from 'src/Components/Navbar'
+const Discografias = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Discografias</h1>
+      </div>
+    </>
+  )
+}
+
+export default Discografias
---
+++ pages/playlists/index.jsx
@@ -0,0 +1,14 @@
+import Navbar from 'src/Components/Navbar'
+
+const PlayLists = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>PlayLists</h1>
+      </div>
+    </>
+  )
+}
+
+export default PlayLists
