43ecff9c5604e33d0fbd9f2d5b3e50703a355faa
mayconbalves <mayconbalves1@gmail.com>
2023-12-13 23:05:41 -0300

    feat(document): added server render to styled components

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,46 +1,75 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  const meta = {
-    title: 'Metal Brasil',
-    description:
-      'Website dedicado ao metal do Brasil e sobre o melhor estilo de música que existe.',
-    image: 'https://mayconbalves.com.br/images/brasil-metal.png',
-    url: 'https://www.metalbrasil.net/'
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@metalbrasil" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-        <meta property="og:url" content={meta.url}></meta>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4370754805788508"
-          strategy="lazyOnload"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.cdnfonts.com/css/metal-mania"
-          rel="stylesheet"
-        />
-      </Head>
-      <title>Metal Brasil</title>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  render() {
+    const meta = {
+      title: 'Metal Brasil',
+      description:
+        'Website dedicado ao metal do Brasil e sobre o melhor estilo de música que existe.',
+      image: 'https://mayconbalves.com.br/images/brasil-metal.png',
+      url: 'https://www.metalbrasil.net/'
+    }
+
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@metalbrasil" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+          <meta property="og:url" content={meta.url}></meta>
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4370754805788508"
+            strategy="lazyOnload"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.cdnfonts.com/css/metal-mania"
+            rel="stylesheet"
+          />
+        </Head>
+        <title>Metal Brasil</title>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
